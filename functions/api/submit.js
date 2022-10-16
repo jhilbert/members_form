0a65862b907eacb9a6a06852c0485820c499eac5
jhilbert <josef.hilbert@icloud.com>
2022-10-16 17:33:40 +0200

    working example

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -5,24 +5,35 @@ export async function onRequestPost({ request }) {
 	try {
 		let input = await request.formData();
 
-		// Convert FormData to JSON
-		// NOTE: Allows mutliple values per key
-		let tmp, output = {};
-		for (let [key, value] of input) {
-			tmp = output[key];
-			if (tmp === undefined) {
-				output[key] = value;
-			} else {
-				output[key] = [].concat(tmp, value);
-			}
-		}
+		const {
+			name,
+			email,
+			referers,
+			movies
+			  } = Object.fromEntries(input)
+		
+			  await MEMBERS.put(email, name);
 
-		let pretty = JSON.stringify(output, null, 2);
-		return new Response(pretty, {
-			headers: {
-				'Content-Type': 'application/json;charset=utf-8'
-			}
-		});
+				const value = await MEMBERS.get("josef.hilbert@icloud.com");
+				if (value === null) {
+				  return new Response("Value not found", { status: 404 });
+				}
+				return new Response(value);
+
+				// Convert FormData to JSON
+		// NOTE: Allows mutliple values per key
+//		let tmp, output = {};
+//		for (let [key, value] of input) {
+//			tmp = output[key];
+//			if (tmp === undefined) {
+//				output[key] = value;
+//			} else {
+//				output[key] = [].concat(tmp, value);
+//			}
+//		}
+
+	//	let pretty = JSON.stringify(output, null, 2);
+
 	} catch (err) {
 		return new Response('Error parsing JSON content', { status: 400 });
 	}
