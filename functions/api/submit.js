a327e2a987c911d873df7b8613675cbb9ab20b82
jhilbert <josef.hilbert@icloud.com>
2022-10-16 21:20:26 +0200

    working example

--- functions/api/submit.js
+++ functions/api/submit.js
@@ -14,11 +14,19 @@ export async function onRequestPost({ request, env }) {
 		
 			  await env.MEMBERS.put(email, name);
 
-				const value = await env.MEMBERS.get("josef.hilbert@icloud.com");
-				if (value === null) {
-				  return new Response("Value not found", { status: 404 });
-				}
-				return new Response(value);
+			  const list = await env.MEMBERS.list();
+			  const keys = list.keys;
+			  if (!keys) {
+				return new Response("List is empty", { status: 404 });
+			  }
+			  const length = keys.length;
+			
+			  for (let i = 0; i < length; i++) {
+				const name = keys[i].name;
+				const value = await MEMBERS.get(name);
+				keys[i].value = value;
+			  }
+			  return new Response(JSON.stringify(keys));
 
 				// Convert FormData to JSON
 		// NOTE: Allows mutliple values per key
